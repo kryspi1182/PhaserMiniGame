2511db8b748e915d884924dee3be97de4321045c
KrystianW <69908722+kryspi1182@users.noreply.github.com>
2021-05-13 21:33:54 +0200

    Boss development
    Death animation, healthbar, transition to boss fight at the end of the level

--- script.js
+++ script.js
@@ -31,6 +31,8 @@ var playerScore = 0;
 var bossFight = 0;
 var bossHealthPoints = 100;
 
+var bossHealthbar = document.getElementById('bossHealthLeft');
+
 function loadHighscores(){
     if(localStorage.getItem("highscores") != null){
         var scores = JSON.parse(localStorage.getItem("highscores"));
@@ -85,6 +87,7 @@ function preload() {
     this.load.image('healOrb', 'sprites/green_ball.png');
     this.load.image('redParticles', 'particles/red.png');
     this.load.image('greenParticles', 'particles/green.png');
+    this.load.image('whiteParticles', 'particles/white.png');
     this.load.image('projectile', 'bullets/bullet06.png');
     this.load.image('droidProjectile', 'bullets/bullet02.png');
     this.load.image('bossProjectile', 'bullets/bullet285.png');
@@ -103,6 +106,7 @@ function preload() {
 var player, platforms;
 var cursors;
 
+var gameWin = 0;
 var gameWidth = 9000;
 var gameHeight = 300;
 
@@ -132,11 +136,14 @@ function endGame(score){
         
     }
     else{
-        var nick = prompt("Gratulacje! Podaj swój nick:", "abc");
-        submitHighscore(score, nick);
-        var restart = confirm("Restart?");
-        if(restart)
-            location.reload(true);
+        setTimeout(function(){
+            var nick = prompt("Gratulacje! Podaj swój nick:", "abc");
+            submitHighscore(score, nick);
+            var restart = confirm("Restart?");
+            if(restart)
+                location.reload(true);
+        }, 12000);
+        
     }
 }
 
@@ -159,6 +166,7 @@ function create() {
     var particlesRed = this.add.particles('redParticles');    
     var particlesGreen = this.add.particles('greenParticles');
     particlesExplosion = this.add.particles('explosion');
+    particlesWhite = this.add.particles('whiteParticles');
     
     generateImages(this, this.physics, 'dmgOrb', false, gameWidth-1000, gameHeight, 300, particlesRed, 1, player);
     
@@ -266,7 +274,7 @@ function create() {
     //createEnemy(this, this.physics, 'droid', 300, 100, 100);
     generateEnemies(this, this.physics, 'droid', gameWidth-1000, gameHeight, 500, 100, 10);
     
-    boss = this.physics.add.sprite(gameWidth-100, 150, 'boss');
+    boss = this.physics.add.sprite(gameWidth-200, 150, 'boss');
     boss.setDataEnabled();
     boss.setBounce(0);
     boss.setCollideWorldBounds(true);
@@ -310,7 +318,7 @@ function update() {
     }
     
     if(player.x > gameWidth - 600 && bossFight === 0){
-        createBoss(this, this.physics, 'boss', gameWidth-100, 150, 100);
+        createBoss(this, this.physics, 'boss', gameWidth-100, 150, bossHealthPoints);
         bossFight = 1;
         this.physics.world.setBounds(gameWidth - 650, 0, 700, gameHeight);
     }
@@ -499,43 +507,41 @@ function destroyPlatform(projectile, platform){
 }
 
 function projectileDamagePlayer(projectile, target){
+    projectile.disableBody(true, true);
     var tmpExplosion = particlesExplosion.createEmitter({
         speed: 100,
         scale: {start: 0.1, end: 0},
         blendMode: 'ADD'
     });
     tmpExplosion.startFollow(target);
-    projectile.disableBody(true, true);
     target.setVelocityX(0);
     setTimeout(function(){damagePlayer(1);
                           tmpExplosion.on = false;}, 100);
 }
 
 function projectileDamageEnemy(projectile, target){
+    projectile.disableBody(true, true);
     var tmpExplosion = particlesExplosion.createEmitter({
         speed: 100,
         scale: {start: 0.1, end: 0},
         blendMode: 'ADD'
     });
     tmpExplosion.startFollow(target);
-    projectile.disableBody(true, true);
     var hp = target.data.get('health');
     if(target === boss){
-        if(--hp <= 0){
-            var intervalsBoss = JSON.parse(target.data.get('interval'));
-            for (var x = 0; x < intervalsBoss.length; x++){
-                clearInterval(intervalsBoss[x]);
-                console.log(intervalsBoss[x]);
-            }
-            target.destroy();
-            playerScore += 5000 + (health * 100);
-            endGame(playerScore);
-            
+        if(--hp < 0 && gameWin === 0){
+            bossDeathAnimation();
+            gameWin = 1;
         }
         else{
             target.data.set('health', hp);
+            bossHealthbar.style.backgroundColor = 'white';
+            setTimeout(function(){bossHealthbar.style.width = (hp/bossHealthPoints)*100 + '%';}, 300);
+            setTimeout(function(){
+                bossHealthbar.style.backgroundColor = 'green';
+            }, 500);
             target.setVelocityX(0);
-            target.setVelocityY(0);
+            //target.setVelocityY(0);
         }
     }
     else{
@@ -645,5 +651,80 @@ function createBoss(parent, parentPhysics, imageString, positionX, positionY, he
     intervals.push(interval3);
     
     boss.data.set('interval', JSON.stringify(intervals));
+    
+}
+
+function bossDeathAnimation(){
+    var intervalsBoss = JSON.parse(boss.data.get('interval'));
+    for (var x = 0; x < intervalsBoss.length; x++){
+        clearInterval(intervalsBoss[x]);
+        console.log(intervalsBoss[x]);
+    }
+    boss.setVelocityY(-50);
+    var explosions = [];
+    setTimeout(function(){
+        explosions[0] = particlesWhite.createEmitter({
+                        speed: 100,
+                        scale: {start: 0.3, end: 0},
+                        blendMode: 'ADD',    
+                    }); 
+        explosions[0].setPosition(
+                        getRandomInt(boss.x-50, boss.x+50), 
+                        getRandomInt(boss.y-30, boss.y+30));
+    }, 500);
+    setTimeout(function(){explosions[0].on = false;}, 7500);
+    
+    setTimeout(function(){
+        explosions[1] = particlesWhite.createEmitter({
+                        speed: 100,
+                        scale: {start: 0.3, end: 0},
+                        blendMode: 'ADD',    
+                    }); 
+        explosions[1].setPosition(
+                        getRandomInt(boss.x-50, boss.x+50), 
+                        getRandomInt(boss.y-30, boss.y+30));
+    }, 1500);
+    setTimeout(function(){explosions[1].on = false;}, 8000);
+    
+    setTimeout(function(){
+        explosions[2] = particlesWhite.createEmitter({
+                        speed: 100,
+                        scale: {start: 0.3, end: 0},
+                        blendMode: 'ADD',    
+                    }); 
+        explosions[2].setPosition(
+                        getRandomInt(boss.x-50, boss.x+50), 
+                        getRandomInt(boss.y-30, boss.y+30));
+    }, 2500);
+    setTimeout(function(){explosions[2].on = false;}, 8500);
+    
+    setTimeout(function(){
+        explosions[3] = particlesWhite.createEmitter({
+                        speed: 100,
+                        scale: {start: 0.3, end: 0},
+                        blendMode: 'ADD',    
+                    }); 
+        explosions[3].setPosition(
+                        getRandomInt(boss.x-50, boss.x+50), 
+                        getRandomInt(boss.y-30, boss.y+30));
+    }, 3500);
+    setTimeout(function(){explosions[3].on = false;}, 9000);
+    
+    setTimeout(function(){
+        explosions[4] = particlesWhite.createEmitter({
+                        speed: 100,
+                        scale: {start: 0.3, end: 0},
+                        blendMode: 'ADD',    
+                    }); 
+        explosions[4].setPosition(
+                        getRandomInt(boss.x-50, boss.x+50), 
+                        getRandomInt(boss.y-30, boss.y+30));
+    }, 4500);
+    setTimeout(function(){explosions[4].on = false;}, 9500);
+            
+    setTimeout(function(){boss.destroy();}, 10000);
+            
+    playerScore += 5000 + (health * 100);
+    endGame(playerScore);
     
 }
